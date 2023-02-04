f5c34f67d1532a189756a3b2140f13c969f86791
admin <admin@site.ru>
2023-02-04 13:51:59 +0500

    Форма работает

--- kommentapp/forms.py
+++ kommentapp/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from .models import KommentModel
 
 class KommentForm(forms.ModelForm):
-    exclude = ("date", )
-    
     class Meta:
         model = KommentModel
-        
+        fields = ('name', 'text')
     
--- kommentapp/views.py
+++ kommentapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import KommentModel
+from .forms import KommentForm
 
 def komments_view(request):
     komments = KommentModel.objects.all()
-    return render(request, 'kommentapp/komments.html', {'komments':komments})
+    if request.method == "POST":
+        form = KommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = KommentForm()
+    else:
+        form = KommentForm()
+    return render(request, 'kommentapp/komments.html', {'komments':komments, 'komment_form':form})
+    
